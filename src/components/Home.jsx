7ed399dc0c55d43d6e4e2ad0c100a763866c1adb
Aidan Roche <aidanroche937@gmail.com>
2023-07-04 20:54:11 -0400

    Fixed home scaling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,30 +15,30 @@ function Home() {
             <img className="rounded-3xl shadow-lg" src="rome.jpg" />
           </div>
           <div className="w-full h-screen absolute top-0 left-0 bg-white/20"></div>
-          <div className="group m-auto flex flex-col justify-center items-center ">
+          <div className="group m-auto flex flex-col justify-center items-center relative z-0">
             <h1 className="sm:text-5xl text-5x font-bold text-primary">
               Aidan Roche
             </h1>
             <h2 className="sm:text-2xl text-2x pt-4 text-primary">
               Computer Science Student at Northeastern University
             </h2>
-            <div className="flex justify-between items-center pt-6 max-w-[200px] w-full z-0">
+            <div className="flex justify-between items-center pt-6 max-w-[200px] w-full">
               <a href="https://www.instagram.com/aroche_37">
                 <FaInstagram
                   size={40}
-                  className="cursor:pointer hover:scale-110 ease-in duration-200"
+                  className="cursor:pointer hover:scale-110 ease-in duration-200 hover:shadow-sm"
                 />
               </a>
               <a href="https://www.linkedin.com/in/aidanroche/">
                 <FaLinkedinIn
                   size={40}
-                  className="cursor:pointer hover:scale-110 ease-in duration-200"
+                  className="cursor:pointer hover:scale-110 ease-in duration-200 hover:shadow-sm"
                 />
               </a>
               <a href="https://github.com/aidanroche3">
                 <FaGithub
                   size={40}
-                  className="cursor:pointer hover:scale-110 ease-in duration-200"
+                  className="cursor:pointer hover:scale-110 ease-in duration-200 hover:shadow-sm"
                 />
               </a>
             </div>
@@ -46,34 +46,35 @@ function Home() {
         </div>
       ) : (
         <>
-          <div id="home" className="w-full h-full m-auto pt-20 p-5 pb-20 ">
-            <img className="rounded-3xl shadow-lg" src="edinburgh.jpg" />
-          </div>
-          <div className="w-full h-full absolute top-0 left-0 bg-white/20">
-            <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center items-center ">
-              <h1 className="text-5x pl-20 font-bold text-gray-100">
+          <div id="home" className="w-full h-screen">
+            <div className="min-h-0 p-10 py-10 flex justify-center">
+              <img className="rounded-3xl shadow-lg" src="edinburgh.jpg" />
+            </div>
+            <div className="w-full h-screen absolute top-0 left-0 bg-white/20"></div>
+            <div className="group m-auto flex flex-col justify-center items-center relative z-0">
+              <h1 className="sm:text-5xl text-3xl font-bold text-primary">
                 Aidan Roche
               </h1>
-              <h2 className="text-3x pt-4  text-gray-100">
+              <h2 className="sm:text-2xl text-2x pt-4 text-primary">
                 Computer Science Student at Northeastern University
               </h2>
               <div className="flex justify-between items-center pt-6 max-w-[200px] w-full">
                 <a href="https://www.instagram.com/aroche_37">
                   <FaInstagram
-                    size={30}
-                    className="cursor:pointer hover:scale-110 ease-in duration-200"
+                    size={40}
+                    className="cursor:pointer hover:scale-110 ease-in duration-200 hover:shadow-sm"
                   />
                 </a>
                 <a href="https://www.linkedin.com/in/aidanroche/">
                   <FaLinkedinIn
-                    size={30}
-                    className="cursor:pointer hover:scale-110 ease-in duration-200"
+                    size={40}
+                    className="cursor:pointer hover:scale-110 ease-in duration-200 hover:shadow-sm"
                   />
                 </a>
                 <a href="https://github.com/aidanroche3">
                   <FaGithub
-                    size={30}
-                    className="cursor:pointer hover:scale-110 ease-in duration-200"
+                    size={40}
+                    className="cursor:pointer hover:scale-110 ease-in duration-200 hover:shadow-sm"
                   />
                 </a>
               </div>
