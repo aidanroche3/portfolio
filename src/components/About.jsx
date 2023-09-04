59dd01ed6f307d454b4ddaffa7d755f04a5a0049
Aidan Roche <aidanroche937@gmail.com>
2023-09-03 20:15:30 -0400

    Added about me paragraph

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,13 +14,13 @@ function About() {
             About Me
           </h1>
           <p className="text-center max-w-[300px]">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            laudantium possimus quisquam, accusamus, tempora officiis illo neque
-            quibusdam amet nam dignissimos. Pariatur necessitatibus quidem
-            consequatur in error accusantium voluptates, quaerat laborum. Illo
-            quisquam quia consequatur repudiandae saepe ad perspiciatis eius
-            unde fugiat. Distinctio ipsam architecto tenetur totam, alias
-            doloribus sunt.
+            I'm a tech enthusiast and aspiring software developer from the
+            Boston area. With an interest in both computer hardware and
+            software, I have built several computers and love solving problems
+            through code. Outside of computers, I love tennis, golf, and
+            geography. Studying interesting maps and travelling the world are
+            two of my greatest passions, as I ultimately hope to develop
+            software with a focus on mapping the world.
           </p>
         </div>
         <Gallery />
