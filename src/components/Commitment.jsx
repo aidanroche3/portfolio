a9312e432bb28b2c5373a49f51a912bc3d9859d6
Aidan Roche <aidanroche937@gmail.com>
2023-09-07 22:30:55 -0400

    Involvement is now scalable (somewhat)

--- src/components/Commitment.jsx
+++ src/components/Commitment.jsx
@@ -1,39 +1,79 @@
-import React from 'react'
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import Reveal from "./Reveal";
 
 function Commitment({ title, dates, description, skills, image }) {
-    return (
-        <div className='flex justify-start items-center pl-20 pb-4'>
-            <div className='max-w-[80vw]'>
-                <div className='flex flex-row justify-center items-center gap-20'>
-                    <div className='flex flex-col justify-center items-center w-[15vw]'>
-                        <h2 className='font-bold text-center text-xl text-primary pb-8'>{title}</h2>
-                        <img src={image} alt={title} width={120} height={120} />
+  const isLargeScreen = useMediaQuery({ query: "(min-width: 760px" });
+
+  return (
+    <Reveal>
+      <div className="flex justify-start items-center sm:pl-20 pb-4">
+        <div className="max-w-[80vw]">
+          {isLargeScreen ? (
+            <div className="flex flex-row justify-center items-center gap-20">
+              <div className="flex flex-col justify-center items-center w-[15vw]">
+                <h2 className="font-bold text-center text-xl text-primary pb-8">
+                  {title}
+                </h2>
+                <img src={image} alt={title} width={120} height={120} />
+              </div>
+              <div className="flex flex-col max-w-[600px]">
+                <h3 className="text-lg text-primary pt-2">{dates}</h3>
+                <p className="text-primary text-md pt-6 pl-4">{description}</p>
+                <div className="flex flex-row items-center gap-3 pl-4 px-2 pt-4">
+                  {Object.entries(skills).map(([name, link]) => (
+                    <div
+                      key={name}
+                      className="tooltip tooltip-top"
+                      data-tip={name}
+                    >
+                      <img
+                        className="pt-2 hover:scale-110 ease-in duration-200"
+                        src={link}
+                        alt={name}
+                        width={40}
+                        height={40}
+                      />
                     </div>
-                    <div className='flex flex-col max-w-[600px]'>
-                        <h3 className='text-lg text-primary pt-2'>
-                            {dates}
-                        </h3>
-                        <p className='text-primary text-md pt-6 pl-4'>
-                            {description}
-                        </p>
-                        <div className="flex flex-row items-center gap-3 pl-4 px-2 pt-4">
-                            {Object.entries(skills).map(([name, link]) => (
-                                <div key={name} className="tooltip tooltip-top" data-tip={name}>
-                                    <img
-                                        className="pt-2 hover:scale-110 ease-in duration-200"
-                                        src={link}
-                                        alt={name}
-                                        width={40}
-                                        height={40}
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                    </div>
+                  ))}
                 </div>
+              </div>
             </div>
+          ) : (
+            <div className="flex flex-col justify-center items-center gap-4">
+              <div className="flex flex-row justify-between items-center w-[85vw] pr-10 pt-8">
+                <h2 className="font-bold text-left text-xl text-primary">
+                  {title}
+                </h2>
+                <img src={image} alt={title} width={80} height={80} />
+              </div>
+              <div className="flex flex-col max-w-[600px]">
+                <h3 className="text-lg text-primary pt-2">{dates}</h3>
+                <p className="text-primary text-md pt-6 pl-4">{description}</p>
+                <div className="flex flex-row items-center gap-3 pl-4 px-2 pt-4">
+                  {Object.entries(skills).map(([name, link]) => (
+                    <div
+                      key={name}
+                      className="tooltip tooltip-top"
+                      data-tip={name}
+                    >
+                      <img
+                        className="pt-2 hover:scale-110 ease-in duration-200"
+                        src={link}
+                        alt={name}
+                        width={40}
+                        height={40}
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-    )
+      </div>
+    </Reveal>
+  );
 }
 
-export default Commitment
+export default Commitment;
--- src/components/Involvement.jsx
+++ src/components/Involvement.jsx
@@ -1,42 +1,47 @@
-import React from 'react'
-import Commitment from './Commitment'
+import React from "react";
+import Commitment from "./Commitment";
+import Reveal from "./Reveal";
 
 function Involvement() {
   return (
-    <div id="involvement" className='w-full min-h-screen bg-white-200 pb-16'>
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="font-bold text-center text-4xl text-primary pb-10">
-          Involvement
-        </h1>
-        <div className="grid grid-cols-1 gap-12">
-          <Commitment
-            title="Northeastern Electric Racing"
-            dates="January 2023 - June 2023"
-            description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-            skills={{
-              TypeScript: "logos/typescript.svg",
-              React: "logos/react.svg",
-              NodeJs: "logos/node.svg",
-              Yarn: "logos/yarn.svg",
-              VSCode: "logos/vscode.svg",
-            }}
-            image="logos/ner.webp" />
-          <Commitment
-            title="Northeastern Electric Racing"
-            dates="January 2023 - June 2023"
-            description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-            skills={{
-              JavaScript: "logos/javascript.svg",
-              React: "logos/react.svg",
-              Tailwindcss: "logos/tailwindcss.svg",
-              NPM: "logos/npm.svg",
-              VSCode: "logos/vscode.svg",
-            }}
-            image="logos/racket.svg" />
+    <Reveal>
+      <div id="involvement" className="min-h-screen m-auto py-4 pb-12 sm:py-12">
+        <div className="flex flex-col justify-center items-center pt-4">
+          <h1 className="font-bold text-center text-4xl text-primary pb-10">
+            Involvement
+          </h1>
+          <div className="grid grid-cols-1 gap-12">
+            <Commitment
+              title="Northeastern Electric Racing"
+              dates="January 2023 - June 2023"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+              skills={{
+                TypeScript: "logos/typescript.svg",
+                React: "logos/react.svg",
+                NodeJs: "logos/node.svg",
+                Yarn: "logos/yarn.svg",
+                VSCode: "logos/vscode.svg",
+              }}
+              image="logos/ner.webp"
+            />
+            <Commitment
+              title="Northeastern Electric Racing"
+              dates="January 2023 - June 2023"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+              skills={{
+                JavaScript: "logos/javascript.svg",
+                React: "logos/react.svg",
+                Tailwindcss: "logos/tailwindcss.svg",
+                NPM: "logos/npm.svg",
+                VSCode: "logos/vscode.svg",
+              }}
+              image="logos/racket.svg"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    </Reveal>
+  );
 }
 
-export default Involvement
+export default Involvement;
